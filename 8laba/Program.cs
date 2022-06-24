f224fe1555389fb043d6f269df630de544de7a7c
Serj Tigranyan <serj-tigranyan@mail.ru>
2022-06-24 18:36:28 +0200

    Добавьте файлы проекта.

---
+++ 8laba/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _8laba
+{
+    class Program
+    {
+
+        static bool IsPalindrome(string str)
+        {
+            return str == new string(str.Reverse().ToArray());
+        }
+
+        static void Main()
+        {
+            const string filePath = "result.txt";
+            Console.Write("Введите строку: ");
+            var str = Console.ReadLine();
+            var result = IsPalindrome(str);
+            var message = "Строка '" + str + "' является палиндромом: " + result;
+
+            Console.WriteLine(message);
+            File.WriteAllText(filePath, message);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 8laba/ProgramBase.cs
@@ -0,0 +1,11 @@
+﻿namespace _8laba
+{
+    internal class ProgramBase
+    {
+
+        static bool IsPalindrome(string str)
+        {
+            return str == new string(str.Reverse().ToArray());
+        }
+    }
+}
